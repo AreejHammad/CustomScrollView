47d8cff135d8f094f391f4feae2e15f7500f7a8d
Hind <hindalzarah@outlook.com>
2023-01-08 09:44:38 +0300

    commit

---
+++ Shared/View/FriendRowView.swift
@@ -0,0 +1,59 @@
+//
+//  FriendRowView.swift
+//  CustomScrollView (iOS)
+//
+//  Created by Balaji on 18/03/21.
+//
+
+import SwiftUI
+
+struct FriendRowView: View {
+    var friend: Friend
+    var body: some View {
+        
+        HStack(spacing: 15){
+            
+            Image(friend.image)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 60, height: 60)
+                .clipShape(Circle())
+            
+            VStack(alignment: .leading, spacing: 8, content: {
+                
+                Text(friend.name)
+                    .fontWeight(.bold)
+                
+                Text(friend.detail)
+                    .font(.caption)
+                    .foregroundColor(.gray)
+            })
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            // Buttons...
+            Button(action: {}, label: {
+                Image(systemName: "message.fill")
+                    .foregroundColor(Color("yellow"))
+                    .padding()
+                    .background(Color("yellow").opacity(0.2))
+                    .clipShape(Circle())
+            })
+            .padding(.trailing,-5)
+            
+            Button(action: {}, label: {
+                Image(systemName: "phone.fill")
+                    .foregroundColor(Color("yellow"))
+                    .padding()
+                    .background(Color("yellow").opacity(0.2))
+                    .clipShape(Circle())
+            })
+        }
+        .padding(.horizontal)
+    }
+}
+
+struct FriendRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
